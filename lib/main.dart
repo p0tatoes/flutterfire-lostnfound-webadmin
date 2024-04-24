bdbd5f04c019c18030fa86c8223a6d42c3abf373
p0tatoes <elojamp@gmail.com>
2024-04-24 23:15:01 +0800

    refactor: renamed files for better segregation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:lostnfound_webadmin/providers/auth.provider.dart';
 import 'package:lostnfound_webadmin/providers/items.provider.dart';
-import 'package:lostnfound_webadmin/views/admin_dashboard.dart';
-import 'package:lostnfound_webadmin/views/login.dart';
+import 'package:lostnfound_webadmin/views/admin_dashboard.view.dart';
+import 'package:lostnfound_webadmin/views/login.view.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
